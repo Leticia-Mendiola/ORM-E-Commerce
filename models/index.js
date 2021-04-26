21649ecc68d7655d73f700cb77c584b25b2457a5
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-26 14:59:15 -0500

    tag belongs to many product through product tag

--- models/index.js
+++ models/index.js
@@ -25,7 +25,13 @@ Product.belongsToMany(Tag, {
 }),
 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: ''
+});
 
 module.exports = {
   Product,
