a25920c0c6fa3eab7cbd30365529bda3f1313900
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-26 14:58:42 -0500

    added products belong to many tags through product tag

--- models/index.js
+++ models/index.js
@@ -16,7 +16,13 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: ''
+}),
 
 // Tags belongToMany Products (through ProductTag)
 
