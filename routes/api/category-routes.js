f542a27130acbdd527ff4098bb2373d0104a0943
Leticia-Mendiola <lmendiola509@gmail.com>
2021-04-26 20:26:09 -0500

    get category by id route in category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,6 +21,14 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
